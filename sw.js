3c60bed38fc3d5f0413cb9cd0cbf6d9e485f0f76
cmuriber <cmuriber@gmail.com>
2026-08-24 14:42:23 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // con IndexedDB (ver app.js, app-manifiesto.js, app-vale.js y
 // app-creditos.js).
 
-const CACHE_NAME = 'r56-dashboard-v24';
+const CACHE_NAME = 'r56-dashboard-v25';
 const SHELL_FILES = [
   './index.html',
   './app.js',
